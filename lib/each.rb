c0741f0b7bedcb4ec50b55ab1cafdea55b56dfa5
sujiko46 <kyosuke.mizukami@jmty.jp>
2020-04-14 11:47:24 +0900

    each

--- lib/each.rb
+++ lib/each.rb
@@ -17,3 +17,9 @@ puts sum#=>10
 non_multiples_of_three = numbers.reject {|n| n % 3 == 0}
 
 non_multiples_of_three #=>[1245]
+
+['ruby','java','perl'].map{|s| s.upcase} #=>['RUBY','JAVA','PERL']
+['ruby','java','perl'].map(&:upcase) #=>['RUBY','JAVA','PERL']
+
+[1,2,3,4,5,6].select{|n|n.odd?} #=>[1,3,5]
+[1,2,3,4,5,6].select(&:odd?) #=>[1,3,5]
