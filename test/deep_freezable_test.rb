dc6bd94e12f7854b8445a1978a63439874f603ed
sujiko46 <kyosuke.mizukami@jmty.jp>
2020-04-17 17:00:14 +0900

    10章おわり

--- test/deep_freezable_test.rb
+++ test/deep_freezable_test.rb
@@ -1,9 +1,11 @@
 require 'minitest/autorun'
 require './lib/bank'
 require './lib/team'
+require 'pry'
 
 class DeepFreezableTest < Minitest::Test
   def test_deep_freeze_to_array
+    binding.pry
     puts Team::COUNTRIES.nil?
     assert_equal %w[Japan US India], Team::COUNTRIES
     assert Team::COUNTRIES.frozen?
@@ -15,5 +17,6 @@ class DeepFreezableTest < Minitest::Test
     assert_equal({ 'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee' }, Bank::CURRENCIES)
     assert Bank::CURRENCIES.frozen?
     assert Bank::CURRENCIES.all? { |key, value| key.frozen? && value.frozen? }
+    #もっと短く
   end
 end
